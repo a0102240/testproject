438493b9240f6f7fdf846b5e09cce0ee0b288e8f
a0102240 <a0102240@naver.com>
2017-05-30 17:34:32 +0900

    ff

--- src/main/java/java24/board/infc/IBoard.java
+++ src/main/java/java24/board/infc/IBoard.java
@@ -2,34 +2,43 @@ package java24.board.infc;
 
 import java.util.List;
 import java24.board.model.ModelArticle;
-import java24.board.model.ModelAttachFile;
 import java24.board.model.ModelBoard;
 import java24.board.model.ModelComments;
 
 public interface IBoard {
-'
-    String getBoardName(String boardcd);
-    ModelBoard getBoardOne (String boardcd);
-    List<ModelBoard> getBoardList();
-    ModelBoard getBoardListResultMap();
-    //bbsResultMap();
-    bbsResultMap getBoardCustomMap();
-    List<ModelBoard> insertBoard(ModelBoard boardcd,ModelBoard boardnm,ModelBoard UseYN,ModelBoard InsertUID,ModelBoard InsertDT,ModelBoard UpdateUID);
-    //updateBoard();
-    int deleteBoard(ModelBoard boardcd,ModelBoard boardnm,ModelBoard UseYN);
-        ModelBoard getBoardSearch(ModelBoard boardcd, ModelBoard boardnm);
-    //ModelBoard getBoardPaging(hashmap boardcd,searchWord, start, end);
-    //insertBoardList java.util.List item.boardcd item.boardnm item.UseYN
-    //int getArticleTotalRecord (boardcd)
-    // ModelArticle getArticleList (boardcd start end);
-     ModelArticle getArticle (int articleNo);
-     List<ModelArticle> insertArticle(ModelArticle boardcd,ModelArticle articleno,ModelArticle title,ModelArticle email,ModelArticle UseYN);
-     List<ModelArticle> updateArticle(ModelArticle articleNo, ModelArticle boardcd, ModelArticle email,ModelArticle UseYN);
-     List<ModelArticle> deleteArticle(ModelArticle articleNo, ModelArticle boardcd, ModelArticle email,ModelArticle UseYN);
-     int increaseHit(int articleNo);
-    
 
+    String getBoardName(String boardcd);
     
+    ModelBoard getBoardOne (String boardcd);
+    
+    List<ModelBoard> getBoardList();
+    
+    ModelBoard getBoardListResultMap();
+    
+    int insertBoard(ModelBoard boardcd,ModelBoard boardnm,ModelBoard UseYN,ModelBoard InsertUID,ModelBoard InsertDT,ModelBoard UpdateUID);
+    
+    int updateBoard();
+    
+    int deleteBoard(ModelBoard boardcd,ModelBoard boardnm,ModelBoard UseYN);
+    
+    List<ModelBoard> getBoardSearch(ModelBoard boardcd, ModelBoard boardnm);
+    
+    List<ModelBoard> getBoardPaging(ModelBoard boardcd, ModelBoard searchWord,ModelBoard start,ModelBoard end);
+    
+    int insertBoardList (int boardcd, int boardnm, int UseYN);
+        
+    int getArticleTotalRecord (int boardcd);
+        
+    List<ModelArticle> getArticleList (ModelArticle boardcd, ModelArticle start, ModelArticle end);
+     
+    ModelArticle getArticle (int articleNo);
+     
+    int insertArticle(ModelArticle boardcd,ModelArticle articleno,ModelArticle title,ModelArticle email,ModelArticle UseYN);
+    
+    int updateArticle(ModelArticle articleNo, ModelArticle boardcd, ModelArticle email,ModelArticle UseYN);
+    
+    int deleteArticle(ModelArticle articleNo, ModelArticle boardcd, ModelArticle email,ModelArticle UseYN);
+
     int increaseHit(int articleNo);
     
     List<ModelArticle> getNextArticle(ModelBoard board, String searchWord); 
