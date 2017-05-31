102a51f92d4e6ddae03aae55de70dbcc717bd89a
toyoil4u <samsungec@gmail.com>
2017-05-31 17:03:47 +0900

    model modified

--- src/main/java/java24/board/model/ModelAttachFile.java
+++ src/main/java/java24/board/model/ModelAttachFile.java
@@ -19,85 +19,85 @@ public class ModelAttachFile {
       
    
 
-public Integer getAttachfileno() {
-    return attachfileno;
-}
-public void setAttachfileno(Integer attachfileno) {
-    this.attachfileno = attachfileno;
-}
-public String getFilename() {
-    return filename;
-}
-public void setFilename(String filename) {
-    this.filename = filename;
-}
-public String getFiletype() {
-    return filetype;
-}
-public void setFiletype(String filetype) {
-    this.filetype = filetype;
-}
-public Integer getFilesize() {
-    return filesize;
-}
-public void setFilesize(Integer filesize) {
-    this.filesize = filesize;
-}
-public Integer getArticleno() {
-    return articleno;
-}
-public void setArticleno(Integer articleno) {
-    this.articleno = articleno;
-}
-public Date getUseYN() {
-    return UseYN;
-}
-public void setUseYN(Date useYN) {
-    UseYN = useYN;
-}
-public String getInsertUID() {
-    return InsertUID;
-}
-public void setInsertUID(String insertUID) {
-    InsertUID = insertUID;
-}
-public String getInsertDT() {
-    return InsertDT;
-}
-public void setInsertDT(String insertDT) {
-    InsertDT = insertDT;
-}
-public String getUpdateUID() {
-    return UpdateUID;
-}
-public void setUpdateUID(String updateUID) {
-    UpdateUID = updateUID;
-}
-public Date getUpdateDT() {
-    return UpdateDT;
-}
-public void setUpdateDT(Date updateDT) {
-    UpdateDT = updateDT;
-}
-public Byte[] getImageData() {
-    return imageData;
-}
-public void setImageData(Byte[] imageData) {
-    this.imageData = imageData;
-}
+    public Integer getAttachfileno() {
+        return attachfileno;
+    }
+    public void setAttachfileno(Integer attachfileno) {
+        this.attachfileno = attachfileno;
+    }
+    public String getFilename() {
+        return filename;
+    }
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+    public String getFiletype() {
+        return filetype;
+    }
+    public void setFiletype(String filetype) {
+        this.filetype = filetype;
+    }
+    public Integer getFilesize() {
+        return filesize;
+    }
+    public void setFilesize(Integer filesize) {
+        this.filesize = filesize;
+    }
+    public Integer getArticleno() {
+        return articleno;
+    }
+    public void setArticleno(Integer articleno) {
+        this.articleno = articleno;
+    }
+    public Date getUseYN() {
+        return UseYN;
+    }
+    public void setUseYN(Date useYN) {
+        UseYN = useYN;
+    }
+    public String getInsertUID() {
+        return InsertUID;
+    }
+    public void setInsertUID(String insertUID) {
+        InsertUID = insertUID;
+    }
+    public String getInsertDT() {
+        return InsertDT;
+    }
+    public void setInsertDT(String insertDT) {
+        InsertDT = insertDT;
+    }
+    public String getUpdateUID() {
+        return UpdateUID;
+    }
+    public void setUpdateUID(String updateUID) {
+        UpdateUID = updateUID;
+    }
+    public Date getUpdateDT() {
+        return UpdateDT;
+    }
+    public void setUpdateDT(Date updateDT) {
+        UpdateDT = updateDT;
+    }
+    public Byte[] getImageData() {
+        return imageData;
+    }
+    public void setImageData(Byte[] imageData) {
+        this.imageData = imageData;
+    }
 
-@Override
-public String toString() {
-    return "ModelAttachfile [attachfileno=" + attachfileno + ", filename="
-            + filename + ", filetype=" + filetype + ", filesize=" + filesize
-            + ", articleno=" + articleno + ", UseYN=" + UseYN + ", InsertUID="
-            + InsertUID + ", InsertDT=" + InsertDT + ", UpdateUID=" + UpdateUID
-            + ", UpdateDT=" + UpdateDT + ", imageData="
-            + Arrays.toString(imageData) + "]";
-}
+    @Override
+    public String toString() {
+        return "ModelAttachfile [attachfileno=" + attachfileno + ", filename="
+                + filename + ", filetype=" + filetype + ", filesize=" + filesize
+                + ", articleno=" + articleno + ", UseYN=" + UseYN + ", InsertUID="
+                + InsertUID + ", InsertDT=" + InsertDT + ", UpdateUID=" + UpdateUID
+                + ", UpdateDT=" + UpdateDT + ", imageData="
+                + Arrays.toString(imageData) + "]";
+    }
 
-public ModelAttachfile() {
-    super();
-}      
+    public ModelAttachFile() {
+        super();
+    }
     
 }
